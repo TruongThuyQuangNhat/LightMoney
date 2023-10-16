8ede3ebad916cde8f8a92388f1d72094dbb227b5
Quang Nhat <truongthuyquangnhat1@gmail.com>
2023-10-16 22:51:31 +0700

    setup calendar

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
+import { NgCalendarModule  } from 'ionic2-calendar';
 import { Tab2PageRoutingModule } from './tab2-routing.module';
 
 @NgModule({
@@ -12,8 +12,8 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
-    Tab2PageRoutingModule
+    Tab2PageRoutingModule,
+    NgCalendarModule
   ],
   declarations: [Tab2Page]
 })
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { CalendarMode } from 'ionic2-calendar';
+import { IView } from 'ionic2-calendar/calendar.interface';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +9,149 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  eventSource: any[] = [];
+  viewTitle: string = '';
 
-  constructor() {}
+  isToday: boolean = true;
+  calendar = {
+    mode: 'month' as CalendarMode,
+    currentDate: new Date(),
+  };
 
+  constructor(private navController: NavController) {}
+
+  loadEvents() {
+    this.eventSource = this.createRandomEvents();
+  }
+
+  onViewTitleChanged(title: string) {
+    this.viewTitle = title;
+  }
+
+  onEventSelected(event: any) {
+    console.log(
+      'Event selected:' +
+        event.startTime +
+        '-' +
+        event.endTime +
+        ',' +
+        event.title
+    );
+  }
+
+  changeMode(mode: CalendarMode) {
+    this.calendar.mode = mode;
+  }
+
+  today() {
+    this.calendar.currentDate = new Date();
+  }
+
+  onTimeSelected(ev: any) {
+    console.log(
+      'Selected time: ' +
+        ev.selectedTime +
+        ', hasEvents: ' +
+        (ev.events !== undefined && ev.events.length !== 0) +
+        ', disabled: ' +
+        ev.disabled
+    );
+  }
+
+  onCurrentDateChanged(event: Date) {
+    var today = new Date();
+    today.setHours(0, 0, 0, 0);
+    event.setHours(0, 0, 0, 0);
+    this.isToday = today.getTime() === event.getTime();
+  }
+
+  createRandomEvents() {
+    var events = [];
+    for (var i = 0; i < 50; i += 1) {
+      var date = new Date();
+      var eventType = Math.floor(Math.random() * 2);
+      var startDay = Math.floor(Math.random() * 90) - 45;
+      var endDay = Math.floor(Math.random() * 2) + startDay;
+      var startTime;
+      var endTime;
+      if (eventType === 0) {
+        startTime = new Date(
+          Date.UTC(
+            date.getUTCFullYear(),
+            date.getUTCMonth(),
+            date.getUTCDate() + startDay
+          )
+        );
+        if (endDay === startDay) {
+          endDay += 1;
+        }
+        endTime = new Date(
+          Date.UTC(
+            date.getUTCFullYear(),
+            date.getUTCMonth(),
+            date.getUTCDate() + endDay
+          )
+        );
+        events.push({
+          title: 'All Day - ' + i,
+          startTime: startTime,
+          endTime: endTime,
+          allDay: true,
+          expenditure: 500,
+          revenue: 3000
+        });
+      } else {
+        var startMinute = Math.floor(Math.random() * 24 * 60);
+        var endMinute = Math.floor(Math.random() * 180) + startMinute;
+        startTime = new Date(
+          date.getFullYear(),
+          date.getMonth(),
+          date.getDate() + startDay,
+          0,
+          date.getMinutes() + startMinute
+        );
+        endTime = new Date(
+          date.getFullYear(),
+          date.getMonth(),
+          date.getDate() + endDay,
+          0,
+          date.getMinutes() + endMinute
+        );
+        events.push({
+          title: 'Event - ' + i,
+          startTime: startTime,
+          endTime: endTime,
+          allDay: false,
+          expenditure: 400,
+          revenue: 7000
+        });
+      }
+    }
+    return events;
+  }
+
+  onRangeChanged(ev: any) {
+    console.log(
+      'range changed: startTime: ' + ev.startTime + ', endTime: ' + ev.endTime
+    );
+  }
+
+  markDisabled = (date: Date) => {
+    var current = new Date();
+    current.setHours(0, 0, 0);
+    return date < current;
+  };
+
+  convertExpenditure(view: any, index: number){
+    return view?.dates[index]?.events?.reduce((acc: number, curr: any) => {
+      return acc + curr.expenditure;
+    }, 0);
+
+  }
+
+  convertRevenue(view: any, index: number){
+    return view?.dates[index]?.events?.reduce((acc: number, curr: any) => {
+      return acc + curr.revenue;
+    }, 0);
+  }
 }
