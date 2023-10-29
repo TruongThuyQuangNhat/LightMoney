e406a1efd7c091c15a1589ff7dc8b3fbac414ef1
Quang Nhat <truongthuyquangnhat1@gmail.com>
2023-10-29 22:52:59 +0700

    add icon and color of category

--- src/app/tab1/detail-category/detail-category.component.ts
+++ src/app/tab1/detail-category/detail-category.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { categoryName, colorArray } from 'src/app/model/category';
 
 @Component({
   selector: 'app-detail-category',
@@ -7,12 +8,24 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./detail-category.component.scss'],
 })
 export class DetailCategoryComponent  implements OnInit {
-
+  categoryName: string[] = [];
+  colorArray: string[] = colorArray;
+  name: string = '';
   constructor(
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
   ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.categoryName = categoryName.filter((item) => item.includes('outline'));
+  }
+
+  filterCate(data?: string){
+    if(data){
+      this.categoryName = categoryName.filter((item) => item.includes(data));
+    }else{
+      this.categoryName = categoryName.filter((item) => !(item.includes('outline') || item.includes('sharp')));
+    }
+  }
 
   back(){
     this.modalCtrl.dismiss();
--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 import { CategoryComponent } from './category/category.component';
 import { DetailCategoryComponent } from './detail-category/detail-category.component';
