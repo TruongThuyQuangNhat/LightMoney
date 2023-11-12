d10a86852289e8cddf689c195e012b2fb375a5ac
Quang Nhat <truongthuyquangnhat1@gmail.com>
2023-11-12 23:35:23 +0700

    add chart

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
-
+import { NgChartsModule } from 'ng2-charts';
 import { Tab3PageRoutingModule } from './tab3-routing.module';
 
 @NgModule({
@@ -11,6 +11,7 @@ import { Tab3PageRoutingModule } from './tab3-routing.module';
     IonicModule,
     CommonModule,
     FormsModule,
+    NgChartsModule,
     Tab3PageRoutingModule
   ],
   declarations: [Tab3Page]
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ChartData, ChartEvent, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,48 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  @Input() type: "revenue" | "expenditure" = "revenue";
   constructor() {}
 
+  changeSegment(data: any) {
+    if(data?.detail?.value){
+      this.type = data.detail.value;
+    }
+  }
+
+  public doughnutChartLabels: string[] = [
+    'Download Sales',
+    'In-Store Sales',
+    'Mail-Order Sales',
+  ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [350, 450, 100] },
+      { data: [50, 150, 120] },
+      { data: [250, 130, 70] },
+    ],
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+
+  // events
+  public chartClicked({
+    event,
+    active,
+  }: {
+    event: ChartEvent;
+    active: object[];
+  }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({
+    event,
+    active,
+  }: {
+    event: ChartEvent;
+    active: object[];
+  }): void {
+    console.log(event, active);
+  }
 }
